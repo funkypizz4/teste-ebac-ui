1a1104b57494dc204e089b36cb8c7643cdd85da3
Padovan <joaopadovan@hotmail.com>
2025-04-16 08:14:44 -0300

    massa de dados em lista

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -12,17 +12,32 @@ describe('Funcionalidade: Produtos', () => {
       cy.get('.product_title').should('contain','Arcadio Gym Short')
     });
     
-   it.only('Deve buscar um produto com sucesso', () => {
+   it('Deve buscar um produto com sucesso', () => {
       let produto = 'Troy Yoga Short';
       produtosPage.buscarProduto(produto);
       cy.get('.product_title').should('contain',produto)
     });
 
     it('Deve visitar a página do produto', () => {
-      
+      produtosPage.visitarProduto('Erica Evercool Sports Bra');
+      cy.get('.product_title').should('contain','Erica Evercool Sports Bra') 
     });
 
     it('Deve adicionar produto ao carrinho', () => {
-      
+      let qtd = 7;
+      produtosPage.buscarProduto('Selene Yoga Hoodie');
+      produtosPage.addProdutoCarrinho('M','Orange',qtd);
+      cy.get('.woocommerce-message').should('contain',`${qtd} × “Selene Yoga Hoodie” foram adicionados no seu carrinho.`);
+    });
+
+    it.only('Deve adicionar produto ao carrinho buscando da massa de dados', () => {
+      cy.fixture('produtos').then(dados => {
+        produtosPage.buscarProduto(dados[0].nomeProduto);
+        produtosPage.addProdutoCarrinho(
+          dados[0].tamanho,
+          dados[0].cor,
+          dados[0].quantidade);
+        cy.get('.woocommerce-message').should('contain', dados[0].nomeProduto);
+      })
     });
 });
